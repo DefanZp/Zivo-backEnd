84f924769b5a8e61780fa1d400b4b5dc785985a0
DefanZp <defanzainp@gmail.com>
2026-08-30 00:44:48 +0700

    - rollback to before cors handle

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,9 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-
-        $middleware->prepend(HandleCors::class);
-        
         $middleware->alias([
             'admin' => App\Http\Middleware\AdminMiddleware::class,
         ]);
